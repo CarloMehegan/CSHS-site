e786cf04ade68bb6e7fce009870c0dee35f1e46f
Carlo <CarloMehegan@gmail.com>
2020-03-06 08:38:19 -0500

    cards are 300px and kind of centered

--- client/src/views/Projects.vue
+++ client/src/views/Projects.vue
@@ -4,14 +4,15 @@
     <h1>Projects</h1>
     <h5>Games and programs made by students inside and outside of class.</h5>
     <br>
-    <div>
-      <b-card-group center deck>
+    <div class = "carddiv">
+      <b-card-group deck columns>
         <b-card
           no-body
           style="max-width: 20rem;"
           img-src="../assets/code3.jpg"
           img-alt="code"
           img-top
+          class = "projectcard"
         >
           <template v-slot:header>
             <h4 class="mb-0">Underwater</h4>
@@ -48,6 +49,7 @@
         img-src="../assets/code2.jpg"
         img-alt="code"
         img-top
+        class = "projectcard"
       >
         <template v-slot:header>
           <h4 class="mb-0">Arcade</h4>
@@ -84,6 +86,7 @@
           img-src="../assets/code3.jpg"
           img-alt="code"
           img-top
+          class = "projectcard"
         >
           <template v-slot:header>
             <h4 class="mb-0">Underwater</h4>
@@ -120,6 +123,119 @@
         img-src="../assets/code2.jpg"
         img-alt="code"
         img-top
+        class = "projectcard"
+      >
+        <template v-slot:header>
+          <h4 class="mb-0">Arcade</h4>
+        </template>
+
+        <b-card-body>
+          <b-card-title>Carlo Mehegan</b-card-title>
+          <b-card-sub-title class="mb-2">October 2, 2019</b-card-sub-title>
+          <b-card-text>
+            This is my final project from Programming & Design. It is an arcade game where you fight peas with a sword.
+          </b-card-text>
+        </b-card-body>
+
+        <b-list-group flush>
+          <b-list-group-item>LÖVE2D</b-list-group-item>
+          <b-list-group-item>Game</b-list-group-item>
+          <b-list-group-item>Programming & Design</b-list-group-item>
+          <b-list-group-item>Arcade</b-list-group-item>
+        </b-list-group>
+
+        <b-card-body>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Carlo's profile</a>
+        </b-card-body>
+
+        <b-card-footer>Uploaded March 5, 2020</b-card-footer>
+
+        <!-- <b-card-img src="../assets/code3.jpg" alt="code" bottom></b-card-img> -->
+      </b-card>
+
+      <b-card
+        no-body
+        style="max-width: 20rem;"
+        img-src="../assets/code2.jpg"
+        img-alt="code"
+        img-top
+        class = "projectcard"
+      >
+        <template v-slot:header>
+          <h4 class="mb-0">Arcade</h4>
+        </template>
+
+        <b-card-body>
+          <b-card-title>Carlo Mehegan</b-card-title>
+          <b-card-sub-title class="mb-2">October 2, 2019</b-card-sub-title>
+          <b-card-text>
+            This is my final project from Programming & Design. It is an arcade game where you fight peas with a sword.
+          </b-card-text>
+        </b-card-body>
+
+        <b-list-group flush>
+          <b-list-group-item>LÖVE2D</b-list-group-item>
+          <b-list-group-item>Game</b-list-group-item>
+          <b-list-group-item>Programming & Design</b-list-group-item>
+          <b-list-group-item>Arcade</b-list-group-item>
+        </b-list-group>
+
+        <b-card-body>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Carlo's profile</a>
+        </b-card-body>
+
+        <b-card-footer>Uploaded March 5, 2020</b-card-footer>
+
+        <!-- <b-card-img src="../assets/code3.jpg" alt="code" bottom></b-card-img> -->
+      </b-card>
+
+
+      <b-card
+        no-body
+        style="max-width: 20rem;"
+        img-src="../assets/code2.jpg"
+        img-alt="code"
+        img-top
+        class = "projectcard"
+      >
+        <template v-slot:header>
+          <h4 class="mb-0">Arcade</h4>
+        </template>
+
+        <b-card-body>
+          <b-card-title>Carlo Mehegan</b-card-title>
+          <b-card-sub-title class="mb-2">October 2, 2019</b-card-sub-title>
+          <b-card-text>
+            This is my final project from Programming & Design. It is an arcade game where you fight peas with a sword.
+          </b-card-text>
+        </b-card-body>
+
+        <b-list-group flush>
+          <b-list-group-item>LÖVE2D</b-list-group-item>
+          <b-list-group-item>Game</b-list-group-item>
+          <b-list-group-item>Programming & Design</b-list-group-item>
+          <b-list-group-item>Arcade</b-list-group-item>
+        </b-list-group>
+
+        <b-card-body>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Carlo's profile</a>
+        </b-card-body>
+
+        <b-card-footer>Uploaded March 5, 2020</b-card-footer>
+
+        <!-- <b-card-img src="../assets/code3.jpg" alt="code" bottom></b-card-img> -->
+      </b-card>
+
+      <b-card
+        no-body
+        style="max-width: 20rem;"
+        img-src="../assets/code2.jpg"
+        img-alt="code"
+        img-top
+        class = "projectcard"
       >
         <template v-slot:header>
           <h4 class="mb-0">Arcade</h4>
@@ -160,14 +276,33 @@
       
     </div>
     
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
   </div>
 </template>
 
 
 <style>
 
-#b-card {
-  padding: 1000px;
+.carddiv {
+  padding-right: 5%;
+  padding-left: 5%;
+  /* margin: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  float: center; */
+  /* float: center; */
+  
+  /* width: 90vw; i HATE THIS HOW DO I CENTER IT*/
+}
+
+.projectcard {
+  min-width: 300px;
+  max-width: 300px;
 }
 
 </style>
