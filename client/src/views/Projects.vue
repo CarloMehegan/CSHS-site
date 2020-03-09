3a93e339eb9f6a3f8bb602a393a22c7472302223
awkenn39 <32737858+awkenn39@users.noreply.github.com>
2020-03-09 13:11:59 -0400

    added array but broke new cards

--- client/src/views/Projects.vue
+++ client/src/views/Projects.vue
@@ -4,14 +4,30 @@
     <h1>Projects</h1>
     <h5>Games and programs made by students inside and outside of class.</h5>
     <br>
-    <div class = "carddiv">
+    <b-button v-on:click="get()">Click Me to Send API Request</b-button>
+    <div class="carddiv">
       <b-card-group deck columns>
         <Project 
+          class="mx-auto"
           title="Hello" 
           name="Aiden" 
           createDate="10-10-10" 
           info="this is a test of our project component" 
-          uploadDate="1-1-1-" 
+          uploadDate="1-1-1" 
+        />
+        <!-- <Project
+          v-if="loaded"
+          class="mx-auto"
+          title="projects[projects.length-1].data.title"
+          name=projects[projects.length-1].data.name
+          createDate=this.projects[projects.length-1].data.createDate
+          info=this.projects[projects.length-1].data.info
+          uploadDate=this.projects[projects.length-1].data.uploadDate
+        /> -->
+        <Project
+          v-for="project in projects"
+          v-bind:key="project.id"
+          v-bind:name="project.name"
         />
       </b-card-group>
     </div>
@@ -19,12 +35,33 @@
 </template>
 
 <script>
+import axios from "axios"
 import Project from '@/components/ProjectComponent'
+
+const URL = "http://10.40.17.230:5000"
 
 export default {
   name:'Projects',
   components: {
     Project
+  },
+  data() {
+    return {
+      loaded: false,
+      projects: [
+      ],
+    }
+  },
+  methods: {
+    get: function () {
+      axios.get(URL + "/api/v1.0/card")
+        .then(response => {
+          this.projects.push(response)
+          console.log(this.projects[this.projects.length - 1].data)
+          this.loaded = true
+          this.projects[this.projects.length - 1].id = this.projects.length - 1
+        })
+    },
   }
 }
 </script>
@@ -35,14 +72,7 @@ export default {
 .carddiv {
   padding-right: 5%;
   padding-left: 5%;
-  /* margin: auto;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  float: center; */
-  /* float: center; */
-  
-  /* width: 90vw; i HATE THIS HOW DO I CENTER IT*/
+
 }
 
 .projectcard {
--- server/app.py
+++ server/app.py
@@ -9,9 +9,9 @@ CORS(app) # avoid Cross-Origin Resource Sharing (CORS) errors
 def index():
     return "Hello, World!" # return this data to the client
 
-@app.route('/api/v1.0/hello', methods=['GET'])
+@app.route('/api/v1.0/card', methods=['GET'])
 def get_tasks():
-    return jsonify({'message': "Hello, client!"})
+    return jsonify({'title': "Test Card",'name': "Aiden Kennedy",'createdDate':"10.10.10",'info':"This is a test of wether or not we can pull from an api",'uploadDate':"12-12-12"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
