c446d5e5249907f4e6dbfc58b8182b82f0976e43
Carlo <CarloMehegan@gmail.com>
2020-05-30 18:12:22 -0400

    created post database
    posts are now stored in a SQLite database, can be read, but cant be created yet

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 CORS(app) # avoid Cross-Origin Resource Sharing (CORS) errors
@@ -9,28 +10,33 @@ CORS(app) # avoid Cross-Origin Resource Sharing (CORS) errors
 def index():
     return "Hello, World!" # return this data to the client
 
-@app.route('/api/v1.0/card', methods=['GET'])
-def get_tasks():
-    return jsonify(
-        {
-            '_id': 1,
-            'title': "Test Card",
-            'name': "Aiden Kennedy",
-            'createDate':"10.10.10",
-            'info':"This is a test of wether or not we can pull from an api",
-            'uploadDate':"12-12-12",
-            'tags': ['python', 'java', 'space']
-        },
-        {
-            '_id': 2,
-            'title': "Test Card 2 the electric boogaloo",
-            'name': "Carlo Megehan",
-            'createDate':"6.9.42",
-            'info':"Test fo' 2 cards",
-            'uploadDate':"3.15.14",
-            'tags': ['python', 'java', 'space', 'horror', 'arcade']
-        }
-    )
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+    
+@app.route('/api/v1.0/posts', methods=['GET'])
+def api_all():
+    conn = sqlite3.connect('posts.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_posts = cur.execute('SELECT * FROM Posts;').fetchall()
+
+    return jsonify(all_posts)
+
+#heres that stuff that wont work
+# @app.route('/api/v1.0/uploadpost', methods=['GET, POST'])
+# def api_post():
+#     conn = sqlite3.connect('posts.db')
+#     cur = conn.cursor()
+#     sqlthing = '''INSERT INTO Posts (name, title, createDate, uploadDate, info) VALUES (?,?,?,?,?)'''
+#     val = ('John', 'Highway 21', '1-2-3', '5-30-20', 'bruh moment pog')
+#     cur.execute(sqlthing, val)
+#     # conn.commit()
+
+#     return cur.lastrowid
+ 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
